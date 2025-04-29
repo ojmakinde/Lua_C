e0d625f56b7d317b1950829e9a706a7914565945
makindeo <makindeo@berea.edu>
2025-04-29 13:14:54 -0400

    Updated Lua stuff

--- example.lua
+++ example.lua
@@ -1,9 +1,9 @@
 a = 0
 b = 1
 count = 0
-print("Enter max fib iteration")
+print("Enter fib number")
 max = io.read("*n")
-while count <= max do
+while count < max do
     print(a)
     count = count + 1
     temp = a + b
